6b65645b7116951cf389993b6265c77bafa26d9e
Suganth-S <suganthsece86@gmail.com>
2021-03-12 13:48:49 +0530

    Ninja ID Project commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,56 +2,94 @@ import 'package:flutter/material.dart';
 
 void main() =>
     runApp(MaterialApp(
-      home: Home()),
-);
+    home: NinjaIdCard(),
+));
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Bismi'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Row(
-          children : <Widget> [
-            Expanded(
-              child: Image.asset('assets/image-1.jpg'),
-              flex: 2,
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Text('1'),
-                color: Colors.cyan,
-                padding: EdgeInsets.all(30.0),
+    class NinjaIdCard extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          backgroundColor: Colors.grey[900],
+          appBar: AppBar(
+            backgroundColor: Colors.grey[850],
+            title: Text('Ninja Id Card'),
+            centerTitle: true,
+            elevation: 0.0,
+          ),
+          body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/ninja.jpg'),
+                  radius: 40.0,
+                ),
               ),
+                Divider(
+                  height: 60.0,
+                  color: Colors.grey[800],
+                ),
+                Text(
+                  'NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0
+                  ),
+                ),
+                SizedBox (height:10.0),
+                Text(
+                  'Suganth S',
+                      style: TextStyle(
+                        color: Colors.amberAccent,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0,
+                      )
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  'CURRENT NINJA LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  '8',
+                  style: TextStyle(
+                    color: Colors.amberAccent,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28.0
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'suganth.s@theninjacircle.co.us',
+                      style : TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0,
+                      ) ,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Text('2'),
-                color: Colors.pink,
-                padding: EdgeInsets.all(30.0),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Text('3'),
-                color: Colors.amber,
-                padding: EdgeInsets.all(30.0),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-    child: Text('click')
-    ),
-    );
-  }
-}
+          ),
+        );
+      }
+    }
+
 
 
