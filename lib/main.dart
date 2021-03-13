2557313ca710b09671b317930295481ce44c775a
Suganth-S <suganthsece86@gmail.com>
2021-03-13 15:51:53 +0530

    Card View List Impl using custom class Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,37 @@ class _ListDataState extends State<ListData> {
     Quote('Shakespeare','Do or Die'),
     Quote('Shakespeare','Do or Die'),
   ];
+
+  Widget quoteTemplate(quote)
+  {
+    return Card(
+        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[800]
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +58,7 @@ class _ListDataState extends State<ListData> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.quote} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
