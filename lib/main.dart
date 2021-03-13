9fe80403be8da87d1801252a4f9079c3e745630b
Suganth-S <suganthsece86@gmail.com>
2021-03-13 13:59:37 +0530

    List Impl Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,110 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() =>
     runApp(MaterialApp(
-    home: NinjaIdCard(),
-));
+      home: ListData() ,
+    ));
 
-    class NinjaIdCard extends StatefulWidget {
+class ListData extends StatefulWidget {
   @override
-  _NinjaIdCardState createState() => _NinjaIdCardState();
+  _ListDataState createState() => _ListDataState();
 }
 
-class _NinjaIdCardState extends State<NinjaIdCard> {
-      int ninjaLevel =0;
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          backgroundColor: Colors.grey[900],
-          appBar: AppBar(
-            backgroundColor: Colors.grey[850],
-            title: Text('Ninja Id Card'),
-            centerTitle: true,
-            elevation: 0.0,
-          ),
-          floatingActionButton: FloatingActionButton
-            (
-            onPressed: (){
-              setState(() {
-                ninjaLevel+=1;
-              });
-            },
-            child: Icon(Icons.add),
-            backgroundColor: Colors.grey[800],
-          ),
-          body: Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/ninja.jpg'),
-                  radius: 40.0,
-                ),
-              ),
-                Divider(
-                  height: 60.0,
-                  color: Colors.grey[800],
-                ),
-                Text(
-                  'NAME',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0
-                  ),
-                ),
-                SizedBox (height:10.0),
-                Text(
-                  'Suganth S',
-                      style: TextStyle(
-                        color: Colors.amberAccent,
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28.0,
-                      )
-                ),
-                SizedBox(height: 30.0),
-                Text(
-                  'CURRENT NINJA LEVEL',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    letterSpacing: 2.0,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Text(
-                  '$ninjaLevel',
-                  style: TextStyle(
-                    color: Colors.amberAccent,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0
-                  ),
-                ),
-                SizedBox(height: 30.0),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.grey[400],
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'suganth.s@theninjacircle.co.us',
-                      style : TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 18.0,
-                        letterSpacing: 1.0,
-                      ) ,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        );
-      }
+class _ListDataState extends State<ListData> {
+  List<String> quotes = [
+    'Do or Die',
+    'Work Hard'
+    'Face Everything and Rise'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('My Favourite Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
 }
+
     
 
     
